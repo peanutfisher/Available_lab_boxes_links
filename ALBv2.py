b312260bffc388c395349f8beb8c1436e867a128
peanutfisher <meifajia@outlook.com>
2024-11-10 14:50:02 +0800

    everything is ok except it is hung, next version to do multi threading

--- ALBv2.py
+++ ALBv2.py
@@ -2,7 +2,7 @@
 Author: peanutfisher meifajia@outlook.com
 Date: 2024-05-19 17:29:05
 LastEditors: peanutfisher meifajia@outlook.com
-LastEditTime: 2024-11-07 14:46:00
+LastEditTime: 2024-11-10 13:38:59
 FilePath: \AvailableLabBox\test.py
 '''
 import logging
@@ -14,7 +14,6 @@ from jinja2 import Environment, FileSystemLoader
 import time
 import os.path
 import json
-from tqdm import tqdm
 import aiohttp
 from aiohttp import ClientTimeout
 import asyncio
@@ -39,7 +38,7 @@ SN_dict = {'202':'PMAX8500', '200':'PMAX2500', '976':'PMAX8000','979':'PMAX2000'
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler('test.log', 'w')
+file_handler = logging.FileHandler('app.log', 'w')
 
 console_handler = logging.StreamHandler()
 #console_handler.setLevel(logging.ERROR)
@@ -65,21 +64,22 @@ def read_html(html):
         logger.info(f'reading {html} into cache...')
 
 def get_link(url):
-    # try:
-    #     html = requests.get(url, verify=False, timeout=5).text
-    #     logger.debug(f'html content: {html}')
-    #     logger.info(f'Connected to {url}')
+    try:
+        html = requests.get(url, verify=False, timeout=5).text
+        #logger.debug(f'html content: {html}')
+        logger.info(f'Connected to {url}')
         
     
-    # except Exception as e:
-    #     logger.error(f'Can not connect to links: {url}, please check your Network connection and your VPN connection!!')
-    #     logger.error('*********************************************************************************************************************')
-    #     logger.error('Please close and retry after you fixed the network.')
-    #     logger.error(str(e))
-    #     #exit_program()
+    except Exception as e:
+        logger.error(f'Can not connect to links: {url}, please check your Network connection and your VPN connection!!')
+        logger.error('*********************************************************************************************************************')
+        logger.error('Please close and retry after you fixed the network.')
+        logger.error(str(e))
+        sg.popup_error(f'Can not connect to links: {url}, please check your Network connection and your VPN connection!!', title='ERROR MESSAGE', font=('Arial', 11))
+        return([],'None')
 
-    # for internal testing
-    html = read_html('html_ra_sample.html')
+    # # for internal testing
+    # html = read_html('html_ra_sample.html')
     
     # use xpath to get all the table content from the website    
     links = etree.HTML(html).xpath('//tr')
@@ -118,12 +118,25 @@ async def check_link(link_list, credential):
     # Prepare progress bar
     total_count = len(link_list)
     logger.info(f'There are total {total_count} items to be checked...')
-    pbar = tqdm(total=total_count)
     
+    # Count for progress bar
     count = 0
+    
+    # Record valid links
+    valid_count = 0
+    
     # check each item in link_list
     for item in link_list:
-        pbar.update(1)
+        count += 1
+                
+        # progress bar
+        pbar = sg.one_line_progress_meter('Checking available links', count, total_count, orientation='h', key='PROGRESSBAR')
+        if not pbar:
+            sg.one_line_progress_meter_cancel('PROGRESSBAR')
+            logger.warning('User cancel the CREATE action, progress bar closed!')
+            sg.popup('CREATE action is cancelled!', title='Warning', font=('Arial', 12))
+            return
+        
         # store the new dict value
         url_dict = {}
         
@@ -149,8 +162,12 @@ async def check_link(link_list, credential):
         # Test the RemoteAnywhere links no matter the SN is.
         result_RA = await test_link(link_RA)
         
+        # add the valid item to raw file
         if result_RA or result_SS:
             raw_file.append(item)
+            # The links is valid so we increase the counter
+            valid_count += 1
+            logger.info(f'This is the {valid_count}th available link.')
 
                 
         
@@ -158,25 +175,26 @@ async def check_link(link_list, credential):
             logger.info(f'Found a available link for SN: {SN}')
             logger.debug(f'Found valid item: {item}')
             
-            count += 1
-            logger.debug(f'Found {count} valid entries')
             
             # store the new dict value, common part for both V3 and V4
-            url_dict['COLOR'] = COLOR[count % 2]
+            url_dict['COLOR'] = COLOR[valid_count % 2]
             url_dict['LABEL'] = item[0]
-            url_dict['SN'] = SN
             url_dict['TYPE'] = model
-            url_dict['RA'] = link_RA
             url_dict['Building'] = item[-5]
             url_dict['Lab'] = item[-4]
             url_dict['Tile'] = item[-3]
             url_dict['Owner'] = item[-2]
             url_dict['Group'] = item[-1]
-            # default empty for V3
+            
+            # RA links information
+            url_dict['RA_SN'] = SN
+            url_dict['RA'] = link_RA
+            # default empty for V4
             url_dict['CS1_SN'] = ''
             url_dict['CS2_SN'] = ''
             url_dict['CS1'] = ''
             url_dict['CS2'] = ''
+            
             # Add value for V4 boxes
             if result_SS:
                 url_dict['CS1_SN'] = SN
@@ -188,9 +206,7 @@ async def check_link(link_list, credential):
             if result_SS:
                 logger.info(f'Found a available link for SN: {SN}')
                 logger.debug(f'Found valid item: {item}')
-            
-                count += 1
-                logger.debug(f'Found {count} valid entries')
+        
             
                 # store the new dict value, common part for both V3 and V4
                 url_dict['COLOR'] = COLOR[count % 2]
@@ -210,19 +226,16 @@ async def check_link(link_list, credential):
                 url_dict['RA_SN'] = ''
                 url_dict['RA'] = ''
             else:
-                pass
-                #logger.error('No available lab box links found!')
+                logger.warning(f'No available lab box links found for {SN}!')
                          
-                
+        
             
         if url_dict:
             html_list.append(url_dict)
             logger.debug(f'url_dict: {url_dict}')
-    
+
     logger.debug(f'html_list: {html_list}')
     
-    # Close progress bar
-    pbar.close()
     
     # Created Raw file for refresh using
     logger.debug(f'raw_file:{raw_file}')
@@ -254,7 +267,7 @@ async def test_link(link):
         #     logger.info(f'reachable url: {link}')
         
         # set the connection timeout 10s for each URL detection
-        timeout = ClientTimeout(total=15)
+        timeout = ClientTimeout(total=18)
         
         async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), timeout=timeout) as session:
         #async with aiohttp.ClientSession() as session:
@@ -301,13 +314,17 @@ def html_table(data, cred):
         
         logging.info(f'{filename} is created under current directory, please check')
         
+        
+        sg.popup(f'Done! The {filename} is created, Click OK to open it in your browser')
+        
         # Open the html with default browser
         cur_dir = os.getcwd()
-        html_path = os.join(cur_dir, filename)
+        html_path = os.path.join(cur_dir, filename)
         webbrowser.open('file://' + html_path)
     
     else:
         logging.warning(f'URL LINK list {data} not available...check check now')
+        sg.popup_error(f'Cannot generate HTML, please check your Network connection and your VPN connection!!', title='ERROR MESSAGE', font=('Arial', 11))
 
 def write_file(listname):
     if listname:
@@ -325,7 +342,7 @@ def get_credential(url, raw_file):
         with open(raw_file, 'r') as f:
             try:
                 old_list = json.load(f)
-                logger.debug(f'Loading list from {raw_file}: {old_list}')
+                #logger.debug(f'Loading list from {raw_file}: {old_list}')
                 
                 # get last CREDENTIAL
                 old_cred = old_list[0][2] or old_list[1][2]
@@ -352,8 +369,13 @@ def get_credential(url, raw_file):
         
 async def generate_html(cred, list_name):
     """Used to generate a latest Credential from web and generate a new html with latest credential"""
+    start_time = time.time()
+    
     html_list = await check_link(list_name, cred)
     html_table(html_list, cred)
+    
+    took_time = time() - start_time
+    logger.info(f'It took about {took_time}s to complete the job')
  
          
 async def main():   
@@ -381,7 +403,7 @@ async def main():
         [sg.Radio('https://phonebook.nexus.cec.delllabs.net/phonebook', font=('Consolas', 11), group_id=2, default=True)],
         [sg.Push()],
         [sg.Push()],
-        
+        [sg.Push()],
         [sg.Text('Latest Credential:', font=('Arial', 13)), sg.Text(text=latest_cred, font=('Consolas', 12), text_color='blue'), 
             sg.VerticalSeparator(),
             sg.Text(text=compare_result, text_color=compare_color),
@@ -390,7 +412,7 @@ async def main():
         
         [sg.Push()],
         [sg.Push()],
-        
+        [sg.Push()],
         [sg.Frame('Button Function', 
             [[sg.Text('CREATE', font=('Arial', 11)), sg.Text('  - will generate a new HTML from above websites', font=('Arial', 9))],
             [sg.Text('REFRESH', font=('Arial', 11)), sg.Text('- refresh the old HTML with new Credential', font=('Arial', 9))],
@@ -408,23 +430,28 @@ async def main():
         if event == sg.WIN_CLOSED or event == 'CANCEL':
             break
         
-        if event == '-CREATE-':
-            await generate_html(latest_cred, latest_list)
+        if latest_list:
+            if event == '-CREATE-':
+                await generate_html(latest_cred, latest_list)
 
-        if event == '-REFRESH-':
-            if old_list:
-                if compare_result == 'SAME':
-                    sg.popup(f"Credential {latest_cred} are latest! No need to do REFRESH", title='Warning', font=('Arial', 11))
+            if event == '-REFRESH-':
+                # check if Raw_Data exists
+                if old_list:
+                    if compare_result == 'SAME':
+                        sg.popup(f"Credential <{latest_cred}> are latest! No need to do REFRESH", title='Warning', font=('Arial', 12))
+                    else:
+                        await generate_html(latest_cred, old_list)
                 else:
-                    await generate_html(latest_cred, old_list)
-            else:
-                sg.popup_error(f"{raw_file_name} NOT found or Corrupted, please use 'CREATE' button to get a new HTML!", title='ERROR MESSAGE', font=('Arial', 11))
-            
-    
-    
+                    sg.popup_error(f"{raw_file_name} NOT found or Corrupted, please use 'CREATE' button to get a new HTML!", title='ERROR MESSAGE', font=('Arial', 11))
+        else:
+            sg.popup_error(f'Cannot connect to links: {url_RA}, please check your Network connection and your VPN connection!!', title='ERROR MESSAGE', font=('Arial', 11))    
+
+
     window.close()
     
     
     
 if __name__ == '__main__':
+
     asyncio.run(main())
+
